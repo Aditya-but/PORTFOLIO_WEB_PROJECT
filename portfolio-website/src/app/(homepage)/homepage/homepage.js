9fbbed5d6d22daa84176d9342161a236361ef5eb
ADITYA RAJ <adityar010@gmail.com>
2024-01-26 17:13:01 +0530

    added a compoment, to check how it works

--- portfolio-website/src/app/(homepage)/homepage/homepage.js
+++
@@ -1,8 +0,0 @@
-
-export default function Homepage(){
-    return(
-        <div>
-            <h1>"DEVELOP"</h1>
-        </div>
-    )
-}
---
+++ portfolio-website/src/app/homepage/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/navbar";
+
+export default function Page() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Hello, Welcome to aditya's website</h1>
+        </div>
+    );
+}
